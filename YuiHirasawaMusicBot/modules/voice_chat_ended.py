cf074dd5984294d7ff564e7d81ae3dea5b8c18ee
Dev3yad <88689277+Dev3yad@users.noreply.github.com>
2021-08-12 09:00:16 +0200

    Add files via upload

--- YuiHirasawaMusicBot/modules/voice_chat_ended.py
+++ YuiHirasawaMusicBot/modules/voice_chat_ended.py
@@ -1,16 +1,14 @@
 from pyrogram import Client
 from pyrogram import filters
 from pyrogram.types import Message
-
+from YuiHirasawaMusicBot.services.callsmusic import client as USER
 from YuiHirasawaMusicBot.services.callsmusic.callsmusic import remove
 from YuiHirasawaMusicBot.helpers.channelmusic import get_chat_id
 
 
 @Client.on_message(filters.voice_chat_ended)
 async def voice_chat_ended(_, message: Message):
-        await message.reply_text(
-            "<b>لقد تم اغلاق المحادثة الصوتية 🙂</b>",
-        )
+        await USER.send_message(message.chat.id, "انضممت هنا كما طلبت")
     try:
         remove(get_chat_id(message.chat))
     except Exception:
