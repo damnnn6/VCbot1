4e7f000e819274ae26efbe1b629dd7055b884778
Dev3yad <88689277+Dev3yad@users.noreply.github.com>
2021-08-27 22:20:54 +0200

    Add files via upload

--- YuiHirasawaMusicBot/modules/song.py
+++ YuiHirasawaMusicBot/modules/song.py
@@ -78,12 +78,12 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-         m.edit("جاري الرفع ")
         rep = f"**🎵 Uploaded by @{BOT_USERNAME}\nBot Channel @{channel}**"
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(dur_arr[i]) * secmul
             secmul *= 60
+         m.edit("جاري الرفع ")
         message.reply_audio(
             audio_file,
             caption=rep,
