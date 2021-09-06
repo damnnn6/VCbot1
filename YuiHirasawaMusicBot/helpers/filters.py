100660fa168f4deddfbe0a74d144ff45133f2ac3
Dev3yad <88689277+Dev3yad@users.noreply.github.com>
2021-09-06 05:42:53 +0200

    Update filters.py

--- YuiHirasawaMusicBot/helpers/filters.py
+++ YuiHirasawaMusicBot/helpers/filters.py
@@ -4,11 +4,5 @@ from typing import Union
 from pyrogram import filters
 
 
-other_filters = filters.group & ~filters.edited & ~filters.via_bot & ~filters.forwarded
-other_filters2 = (
-    filters.private & ~filters.edited & ~filters.via_bot & ~filters.forwarded
-)
-
-
 def command(commands: Union[str, List[str]]):
     return filters.command(commands)
